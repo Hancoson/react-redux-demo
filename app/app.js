4a1dcf5c2678ab8f8dae8da663acc840b0814549
Hancoson <hancoson@163.com>
2017-03-21 17:06:44 +0800

    代码规范

--- app/app.js
+++ app/app.js
@@ -3,12 +3,12 @@
  * @time 2016/12/29.
  */
 import React from 'react'  // 引入React
-import {render} from 'react-dom' // 引入render方法
-import {Provider} from 'react-redux' // 利用Provider可以使我们的 store 能为下面的组件所用
-import {Router, hashHistory} from 'react-router' // Browser history 是由 React Router 创建浏览器应用推荐的 history
+import { render } from 'react-dom' // 引入render方法
+import { Provider } from 'react-redux' // 利用Provider可以使我们的 store 能为下面的组件所用
+import { Router, hashHistory } from 'react-router' // Browser history 是由 React Router 创建浏览器应用推荐的 history
 //使用 hashHistory，浏览器上看到的 url 会是这样的: /#/a/a1?_k=adseis
 //使用 browserHistory,需要服务端的支持，浏览器上看到的 url 会是这样的：/a/a1
-import {syncHistoryWithStore} from 'react-router-redux' // 利用react-router-redux提供的syncHistoryWithStore我们可以结合store同步导航事件 
+import { syncHistoryWithStore } from 'react-router-redux' // 利用react-router-redux提供的syncHistoryWithStore我们可以结合store同步导航事件 
 
 import finalCreateStore from './store/configureStore'  //引入store配置
 import reducer from './reducers'  // 引入reducers集合
@@ -24,7 +24,7 @@ const history = syncHistoryWithStore(hashHistory, store)
 
 render(
   <Provider store={store}>
-    <Router history={history} routes={routes}/>
+    <Router history={history} routes={routes} />
   </Provider>,
   document.getElementById('app')
 )
