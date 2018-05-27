3cc69c171fbd1fbd062f29438e707243c79f12fd
Hancoson <hancoson@163.com>
2018-05-27 15:25:56 +0800

    add saga

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -2,19 +2,19 @@
  * @author Guoxing.Han(hancoson#163.com)
  * @time 2017/1/3.
  */
-import thunk from 'redux-thunk' // redux-thunk 支持 dispatch function，并且可以异步调用它
+import createSagaMiddleware from 'redux-saga'
 import { createStore, applyMiddleware, compose } from 'redux' // 引入redux createStore、中间件及compose
 import createLogger from 'redux-logger' // 利用redux-logger打印日志
 let middleware = '';
 if (process.env.NODE_ENV == 'production') {
   // 创建一个中间件集合
-  middleware = [thunk]
+  middleware = [createSagaMiddleware]
 }
 else {
   // 调用日志打印方法
   const loggerMiddleware = createLogger()
   // 创建一个中间件集合
-  middleware = [thunk, loggerMiddleware]
+  middleware = [createSagaMiddleware, loggerMiddleware]
 }
 
 // 利用compose增强store，这个 store 与 applyMiddleware 和 redux-devtools 一起使用
